ede382c4342956b2ff96ef7bb7ada3457826e893
Yuri Shadrin <ymshadrin@gmail.com>
2023-08-03 15:18:29 +0300

    Open API introduced

--- src/main/java/dev/example/jwtdemo/controller/HelloWorldController.java
+++ src/main/java/dev/example/jwtdemo/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package dev.example.jwtdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,17 +8,18 @@ import dev.example.jwtdemo.model.Role;
 import jakarta.annotation.security.RolesAllowed;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	// allowed for USER and ADMIN
-	@RequestMapping({ "/hello/user" })
+	@GetMapping({ "/user" })
 	@RolesAllowed({Role.USER_ROLE})
 	public String helloUser() {
 		return "Hello USER!";
 	}
 
 	// allowed for ADMIN only
-	@RequestMapping({ "/hello/admin" })
+	@GetMapping({ "/admin" })
 	@RolesAllowed({Role.ADMIN_ROLE})
 	public String helloAdmin() {
 		return "Hello ADMIN!";
